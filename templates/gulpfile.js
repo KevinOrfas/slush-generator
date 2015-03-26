e9d29972dce23546d0b3edf5331bd04d009ec1a0
Anastasios Orfanidis <anastasios.orfanidis@wardour.co.uk>
2015-03-26 15:22:38 +0000

    Using gulp-load-plugins

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,22 +1,25 @@
 var gulp = require('gulp');
 
-var gutil = require('gulp-util'),
-    jshint = require('gulp-jshint'),
-    sass   = require('gulp-sass'),
-    concat = require('gulp-concat'),
-    sourcemaps = require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    htmlmin = require('gulp-htmlmin'),
-    rename = require('gulp-rename'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload'),
-    del = require('del'),
-    connect = require('gulp-connect'),
-    size = require('gulp-size');
+var $ = require('gulp-load-plugins')();
+
+// var gutil = require('gulp-util'),
+//     jshint = require('gulp-jshint'),
+//     sass   = require('gulp-sass'),
+//     concat = require('gulp-concat'),
+//     sourcemaps = require('gulp-sourcemaps'),
+//     autoprefixer = require('gulp-autoprefixer'),
+//     minifycss = require('gulp-minify-css'),
+//     uglify = require('gulp-uglify'),
+//     imagemin = require('gulp-imagemin'),
+//     htmlmin = require('gulp-htmlmin'),
+//     rename = require('gulp-rename'),
+//     notify = require('gulp-notify'),
+//     cache = require('gulp-cache'),
+//     livereload = require('gulp-livereload'),
+//     del = require('del'),
+//     connect = require('gulp-connect'),
+//     size = require('gulp-size');
+
 
 
 var browserSync = require('browser-sync'),
@@ -64,62 +67,62 @@ gulp.task('watch', function () {
 
 gulp.task('sass', function() {
   return gulp.src(input.sass)
-    .pipe(sass())
-    .pipe(autoprefixer('last 2 version'))
+    .pipe($.sass())
+    .pipe($.autoprefixer('last 2 version'))
     .pipe(gulp.dest(output.css))
     .pipe(reload({stream: true}))
-    .pipe(notify({ message: 'CSS in the folder' }));
+    .pipe($.notify({ message: 'CSS in the folder' }));
 });
 
 // configure the jshint task
 gulp.task('jshint', function() {
   return gulp.src(input.scripts)
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('build-css', function() {
   return gulp.src(input.sass)
-    .pipe(sourcemaps.init()) // Process the original sources
-      .pipe(sass({ style: 'expanded' }))
-      .pipe(autoprefixer('last 2 version'))
-      .pipe(minifycss())
-      .pipe(rename({suffix: '.min'}))
-    .pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe($.sourcemaps.init()) // Process the original sources
+      .pipe($.sass({ style: 'expanded' }))
+      .pipe($.autoprefixer('last 2 version'))
+      .pipe($.minifycss())
+      .pipe($.rename({suffix: '.min'}))
+    .pipe($.sourcemaps.write()) // Add the map to modified source.
     .pipe(gulp.dest(output.styles))
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe($.notify({ message: 'Styles task complete' }));
 });
 
 
 gulp.task('build-js', function() {
   return gulp.src([input.scripts, input.vendor])
-    .pipe(sourcemaps.init())
-      .pipe(concat('bundle.js'))
+    .pipe($.sourcemaps.init())
+      .pipe($.concat('bundle.js'))
       //only uglify if gulp is ran with '--type production'
-      .pipe(gutil.env.type === 'production' ? rename({suffix: '.min'}) : gutil.noop()) 
-      .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop()) 
-    .pipe(sourcemaps.write())
+      .pipe($.gutil.env.type === 'production' ? $.rename({suffix: '.min'}) : $.gutil.noop()) 
+      .pipe($.gutil.env.type === 'production' ? $.uglify() : $.gutil.noop()) 
+    .pipe($.sourcemaps.write())
     .pipe(gulp.dest(output.scripts))
-    .pipe(notify({ message: 'Scripts task complete' }));;
+    .pipe($.notify({ message: 'Scripts task complete' }));;
 });
 
 gulp.task('html', function () {
   gulp.src('./*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe($.htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('images', function() {
   return gulp.src(input.images)
-    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe($.cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
     .pipe(gulp.dest(output.images))
-    .pipe(notify({ message: 'Images task complete' }));
+    .pipe($.notify({ message: 'Images task complete' }));
 });
 
 gulp.task('graphics', function() {
   return gulp.src(input.graphics)
     .pipe(gulp.dest(output.graphics))
-    .pipe(notify({ message: 'Graphics task complete' }));
+    .pipe($.notify({ message: 'Graphics task complete' }));
 });
 
 // gulp.task('fonts', function () {
@@ -136,7 +139,7 @@ gulp.task('clean', function(cb) {
 
 
 gulp.task('build', ['build-css', 'build-js', 'html', 'images', 'graphics'], function () {
-  return gulp.src('dist/**/*').pipe(size({title: 'build', gzip: true}));
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
 gulp.task('default', ['clean'], function() {
